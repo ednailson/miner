fb802618d4a6d3b6d9d21fde335cb1bc37dc6f72
ednailson <evbcjr@gmail.com>
2021-10-23 12:55:14 -0300

    feat: small improvements

--- internal/domain/usecase/miner.go
+++ internal/domain/usecase/miner.go
@@ -22,14 +22,12 @@ func (u *useCaseMiner) Save(req entity.Request) interface{} {
 	case entity.AuthorizeMethod:
 		result = true
 	case entity.SubscribeMethod:
-		switch req.Params.(type) {
-		case []interface{}:
-			params, _ := req.Params.([]interface{})
+		switch params := req.Params.(type) {
+		case []string:
 			if len(params) != 1 || params[0] != minerVersion {
 				return entity.NewFail(&req.ID, entity.ErrorInvalidParams())
 			}
 		case map[string]interface{}:
-			params, _ := req.Params.(map[string]interface{})
 			if value, ok := params["version"]; !ok || value != minerVersion {
 				return entity.NewFail(&req.ID, entity.ErrorInvalidParams())
 			}
